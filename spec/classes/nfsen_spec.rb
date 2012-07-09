ec40533ca23273a1f407baa6547d8771430d0544
Geoff Davis <gadavis@ucsd.edu>
2012-07-09 14:57:34 -0700

    Add basic test of nfsen class

--- spec/classes/nfsen_spec.rb
+++ spec/classes/nfsen_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe 'nfsen' do
+
+  describe 'nfsen with no parameters, basic test' do
+    let (:params) { { } }
+
+    it { should create_class('nfsen') }
+  end
 end

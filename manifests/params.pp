7279810a709bfc45c9f48e21669250085632cbc1
Geoff Davis <gadavis@ucsd.edu>
2012-07-09 17:15:52 -0700

    Copy default nfsen.conf and set up service on Solaris only
    This is based on our existing config on anfmon.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,2 +1,14 @@
 class nfsen::params {
+  case $::osfamily {
+    'Solaris': {
+      $netflow_basedir='/opt/netflow'
+      $init_script='/var/svc/manifest/site/nfsen.xml'
+      $init_template='nfsen/smf/nfsen.xml.erb'
+      $svc='svc:/network/nfsen:default'
+      $conf_source="puppet:///modules/${module_name}/nfsen.conf"
+    }
+    default: {
+      fail("Unsupported OS: $::operatingsystem")
+    }
+  }
 }
